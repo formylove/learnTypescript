f8c9529b610dc507f861eea42b805b69ea5a9efc
formylove <gigglepilate@gmail.com>
2020-01-11 10:36:28 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.info("hello");
+
+function print(s:string){
+
+console.error(s)
+
+}
+var str:string='你好ts'
+
+print(str)
---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.info("hello");
+function print(s) {
+    console.error(s);
+}
+var str = '你好ts';
+print(str);
